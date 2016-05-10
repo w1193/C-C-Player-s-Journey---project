f3ca34d384db76dda0273a5a3a226d9ef99b8171
unguys33 <gus2065@naver.com>
2016-05-10 18:21:35 +0900

    InitialScreen.cpp

---
+++ InitialScreen.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main() {
+
+	printf("기숙사\n\n\n");
+	printf("눈을 떳다.\n\n");
+	printf("무엇을 하시겠습니까?\n\n\n");
+	printf("1. 잔다.\n\n");
+	printf("2. 씻는다.\n\n");
+	printf("3. 나간다.\n\n");
+
+	getchar();
+}
