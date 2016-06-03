546ce1bfcf22a84c491d465a0fcdfbf4637d3ecf
ssoso27 <sohi8290@naver.com>
2016-06-04 03:00:22 +0900

    닷지

--- Dodge/닷지.cpp
+++ Dodge/닷지.cpp
@@ -5,6 +5,10 @@
 #include "Screen.c"
 #pragma warning(disable:4996)
 
+#define DotNum 10
+#define BOARD_WIDTH 35       // 게임 영역의 가로(열) >= 10 : ━
+#define BOARD_HEIGHT 20      // 게임 영역의 세로(행) >= 20 : | 
+
 // 열거형 선언
 
 enum ControlKeys
@@ -33,55 +37,12 @@ typedef struct _DOT
 }DOT;
 
 PLAYER Player;
-DOT Dot[100];
+DOT Dot[DotNum];
 
 // 플레이어와 점의 모양 
 
-char Player_Shape[] = "*";
+char Player_Shape[] = {"*"};
 char Dot_Shape[] = "·";
-
-/*
-int MoveFunction(int x, int y)
-{
-	int a, k;
-	a=
-	y = a*x + k;
-	return y;
-}
-*/
-
-void Init()
-{
-	/*
-	srand((unsigned)time(0)); // rand() 함수로 랜덤값을 주기 위해서 초기값 부여
-
-	int i;
-	for (i = 0; i < 100; i++)
-	{
-		DOT[i].A = rand();
-	}
-	*/
-
-	// PLAYER 초기화
-
-	Player.X = Player.Y = 30;
-}
-
-void Update()
-{
-
-}
-
-void Render()
-{
-	ScreenClear();
-
-}
-
-void Release()
-{
-
-}
 
 void KeyControl(int key)
 {
@@ -90,7 +51,6 @@ void KeyControl(int key)
 	{
 	case UP:
 		Player.Y--;
-		printf("Up");
 		break;
 
 	case DOWN:
@@ -104,11 +64,134 @@ void KeyControl(int key)
 	case RIGHT:
 		Player.X++;
 		break;
-
+/*
 	case SPACE:
 
 		break;
+		*/
 	}
+}
+
+void TimeTerm()
+{
+	clock_t CurTime, OldTime;
+
+	OldTime = clock();
+	while (1)
+	{
+		CurTime = clock();
+
+		if (CurTime - OldTime > 0.1 * 1000)
+		{
+			break;
+		}
+	}
+}
+void DrawField(void) // 필드 틀 제시. (수정 필요)
+{
+	int x, y;
+
+	//위 보드 라인
+	for (x = 1; x < BOARD_WIDTH ; x++)
+	{
+		ScreenPrint(x,0," ━ ");
+	}
+	//아래 보드 라인
+	for (x = 1; x < BOARD_WIDTH ; x++)
+	{
+		ScreenPrint(x, BOARD_HEIGHT, " ━ ");
+	}
+	//왼쪽 보드 라인
+	for (y = 0; y < BOARD_HEIGHT + 1; y++)
+	{
+
+		if (y == BOARD_HEIGHT)
+			ScreenPrint(0, y, "┗");
+		else if (y == 0)
+			ScreenPrint(0, y, "┏");
+		else
+			ScreenPrint(0,y,"┃");
+	}
+	//오른쪽 보드 라인
+	for (y = 0; y < BOARD_HEIGHT + 1; y++)
+	{
+		if (y == BOARD_HEIGHT)
+			ScreenPrint(BOARD_WIDTH, y, "┛");
+		else if (y ==0)
+			ScreenPrint(BOARD_WIDTH, y, "┓");
+		else
+			ScreenPrint(BOARD_WIDTH,y,"┃");
+	}
+}
+
+// 프레임워크 함수 시작
+
+void Init()
+{
+	// 필드 표시
+
+	// PLAYER 초기화
+
+	Player.X =10;
+	Player.Y = 10;
+
+	// DOT[] 초기화
+	
+	//int i=0;
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i < DotNum; i++)
+	{
+		Dot[i].A = rand()%9;
+		Dot[i].K = rand()%9;
+		
+		Dot[i].X = 0;
+		Dot[i].Y = Dot[i].A * Dot[i].X + Dot[i].K;
+	}
+	
+}
+
+void Update()
+{
+	for (int i = 0; i < DotNum; i++)
+	{
+		Dot[i].X++;
+	}
+
+	for (int i = 0; i < DotNum; i++)
+	{
+		if (Dot[i].X > BOARD_WIDTH)
+		{
+			Dot[i].X = Dot[i].Y=0;
+		}
+		if (Dot[i].Y > BOARD_HEIGHT || Dot[i].Y < 0)
+		{
+			Dot[i].X = Dot[i].Y = 0;
+		}
+
+	}
+}
+
+void Render()
+{
+	ScreenClear();
+
+	// 화면 표시
+	DrawField();
+
+	ScreenPrint(Player.X, Player.Y, Player_Shape); // 플레이어 표시
+
+	for (int i = 0; i < DotNum; i++)
+	{
+		ScreenPrint(Dot[i].X, Dot[i].Y, Dot_Shape); // 점 표시
+	}
+
+	ScreenFlipping();
+}
+
+void Release()
+{
+
 }
 
 void main()
@@ -132,6 +215,8 @@ void main()
 		}
 		Update();
 		Render();
+
+	//	TimeTerm();
 	}
 
 	Release();
